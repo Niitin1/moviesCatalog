a11835f6aa3b90ff96795af16b9a870bf090ebda
Nitin <nksinghal41@gmail.com>
2020-08-29 12:50:58 +0530

    t1

---
+++ src/main/java/io/nitinLearn/moviescatalogservice/controller/HomeController.java
@@ -0,0 +1,15 @@
+package io.nitinLearn.moviescatalogservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping(value = "/")
+	public String getTest() {
+		return "movieCatalog";
+	}
+	
+
+}
