6bd052f84b76d51bcc44581580c020b1d9fa6437
Nitin <nksinghal41@gmail.com>
2020-08-30 12:59:12 +0530

    ReactiveProgramming using WebClient.Builder (asynchronous)

--- src/main/java/io/nitinLearn/moviescatalogservice/service/HomeService.java
+++ src/main/java/io/nitinLearn/moviescatalogservice/service/HomeService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import io.nitinLearn.moviescatalogservice.bean.HomeBean;
 import io.nitinLearn.moviescatalogservice.bean.Movie;
@@ -17,39 +18,34 @@ import io.nitinLearn.moviescatalogservice.bean.Rating;
 
 @Service
 public class HomeService {
-	
+
 	@Autowired
 	RestTemplate restTemplate;
-	
+
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+
 	@Value("${movieInfoUrl}")
 	String url;
-	
+
 	private final Logger log = Logger.getLogger(this.getClass());
 
 	public List<HomeBean> getCatalog(String userId) {
 		// TODO Auto-generated method stub
-		log.info("service has been called "+userId);
-		
-	//	RestTemplate restTemplate = new RestTemplate();
-		//log.info("url is "+url);
-		//Movie movie = restTemplate.getForObject("http://localhost:8082/movies-info-service", Movie.class);
-		
-		
+		log.info("service has been called " + userId);
+
 		List<Rating> ratings = new ArrayList<Rating>();
-		ratings.add(new Rating("1234",4));
+		ratings.add(new Rating("1234", 4));
 		ratings.add(new Rating("5678", 3));
-		
-		return ratings.stream().map(rating ->{
-			Movie movie =	restTemplate.getForObject(url+rating.getMovieId(), Movie.class); 
-			return new HomeBean(movie.getName(), movie.getMovieId()  , rating.getRating());
-			}).collect(Collectors.toList());
-		}
-		
-	//	return ratings.stream().map(rating -> new HomeBean("Transformer", "Test" , 4)).collect(Collectors.toList());
-		
-		/*
-		 * HomeBean hb = new HomeBean("Transformer", "Test" , 4); List<HomeBean> hbs =
-		 * new ArrayList<HomeBean>(); hbs.add(hb); return hbs;
-		 */	
+
+		return ratings.stream().map(rating -> {
+			// Movie movie = restTemplate.getForObject(url+rating.getMovieId(),
+			// Movie.class);
+
+			// reactive programming(asynchronous programming)
+			Movie movie = webClientBuilder.build().get().uri(url).retrieve().bodyToMono(Movie.class).block();
+			return new HomeBean(movie.getName(), movie.getMovieId(), rating.getRating());
+		}).collect(Collectors.toList());
+	}
 
 }
