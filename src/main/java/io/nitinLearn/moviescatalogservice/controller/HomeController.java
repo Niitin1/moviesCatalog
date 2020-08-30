2b52d18662e22753578f5122860a3c0c6fc306f6
Nitin <nksinghal41@gmail.com>
2020-08-30 10:33:53 +0530

    basic api to fetch data

--- src/main/java/io/nitinLearn/moviescatalogservice/controller/HomeController.java
+++ src/main/java/io/nitinLearn/moviescatalogservice/controller/HomeController.java
@@ -1,10 +1,16 @@
 package io.nitinLearn.moviescatalogservice.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.apache.log4j.Logger;
 
+import io.nitinLearn.moviescatalogservice.bean.HomeBean;
 import io.nitinLearn.moviescatalogservice.service.HomeService;
 
 @RestController
@@ -21,5 +27,13 @@ public class HomeController {
 		return "movieCatalog1";
 	}
 	
-
+	@GetMapping(value = "/getCatalog{userId}")
+	private @ResponseBody List<HomeBean> getCatalog(@PathVariable("userId") String userId){
+		log.info("userID  "+userId);
+		HomeBean hb = new HomeBean("Transformer", "Test" , 4);
+		List<HomeBean> hbs =  new ArrayList<HomeBean>();
+		hbs.add(hb);
+		return hbs;
+	}
+	
 }
